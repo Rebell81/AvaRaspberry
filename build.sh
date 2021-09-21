4c709ff4a3ef7a5fcb55640396c2dd8b660f3e1f
rebell81 <sharkcffg@gmail.com>
2021-09-21 11:43:41 +0300

    d

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Started build and run project script."
-
+echo "$SECRET_PASSPHRASE"
+echo $SECRET_PASSPHRASE
 # Configure linux machine
 
 # Explicitly setting DISPLAY=:0 is usually a way to access a machine's local display from outside the local session
@@ -14,8 +15,7 @@ dotnet build --configuration Release
 cd AvaRaspberry/bin/Release/net5.0/ || exit
 
 # Decrypt AppSettings config with help Github Secrets 
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output appsettings.json appsettings.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output appsettings.json appsettings.json.gpg
 
 # Run
 ( dotnet AvaRaspberry.dll & )
